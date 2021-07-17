1dc8f7ded13dc2775425c002b2c7b4f15bcbe7a1
ngocnamle <87576879+ngocnamle@users.noreply.github.com>
2021-07-17 22:45:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,6 +13,6 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'sang tuần anh qua đón đi lượn nhé công chúa.',
+    messDesc: 'Sang tuần anh qua đón đi lượn nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: https://www.facebook.com/ngocnam0306
